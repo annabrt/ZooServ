640e74a4e52b57b2b63249267ce3868dd179aada
Anna Bertochi <anafbrtc@gmail.com>
2024-12-15 22:15:42 -0300

    Add files via upload

---
+++ projeto zoo html-css/script.js
@@ -0,0 +1,25 @@
+let login = document.querySelector
+
+var swiper = new Swiper(".galeria-slide", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    spaceBetween:20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0:{
+            slidesPerView:1,
+        },
+        700:{
+            slidesPerView:2,
+        },
+    }
+
+}
+
+
+
+)
